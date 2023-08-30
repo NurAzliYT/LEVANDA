b100acd59093d4fc6cc35b2ee6923ab1312477c7
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-08-31 05:18:28 +0700

    Update LevandaUI.php

--- LevandaUI/src/NurAzliYT/LevandaUI/LevandaUI/LevandaUI.php
+++ LevandaUI/src/NurAzliYT/LevandaUI/LevandaUI/LevandaUI.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace LevandaUI/src/NurAzliYT/LevandaUI/LevandaUI.php
+namespace LevandaUI\src\NurAzliYT\LevandaUI\LevandaUI.php
 
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
