b494aab927f14d2bfb6661bdf6df4957b7e5cfac
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-09-06 14:56:26 +0700

    Update FormAPISettings.php

--- FormAPI/src/jojoe77777/FormAPI/FormAPISettings.php
+++ FormAPI/src/jojoe77777/FormAPI/FormAPISettings.php
@@ -3,6 +3,26 @@
 namespace jojoe77777\FormAPI\FormAPISettings;
 
 use jojoe\FormAPI\SimpleForm;
+use pocketmine\utils\Config;
+use function in_array;
 
 class FormAPISettings{
+  
+	private static ?FormAPI $plugin;
+	private static ?Config $config;
+	private static ?Config $formapi;
+
+	private function __construct(){}
+
+	public static function init(ScoreHud $plugin): void{
+		self::$plugin = $plugin;
+		self::$config = $plugin->getConfig();
+		self::$formapi= $plugin->getFormConfig();
+	}
+
+	public static function destroy(): void{
+		self::$plugin = null;
+		self::$config = null;
+		self::$formapi = null;
+  }
 }
