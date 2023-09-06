cfe4cd62b5afc4dee303345c7321bbf3899c023f
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-09-06 17:03:47 +0700

    Update LevandaUISetting.php

--- LevandaUI/src/NurAzliYT/LevandaUI/LevandaUI/LevandaUISetting.php
+++ LevandaUI/src/NurAzliYT/LevandaUI/LevandaUI/LevandaUISetting.php
@@ -1,12 +1,1 @@
-<?php
 
-namespace NurAzliYT\LevandaUI\LevandaUI;
-
-use NurAzliYT\LevandaUI\LevandaUI;
-
-  class LevandaUISetting {
-
-    $plugin->getLogger()->info("Klik Disini Karena Kamu Hanya Bisa Mendapatkannya Sekali Sebulan");
-
-
-  }
