ad051c5830dd3a7dfd19e96e17d7b5d31c463a5b
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-10-09 16:44:04 +0700

    Update LevandaUI.php

--- LevandaUI/src/NurAzliYT/LevandaUI/LevandaUI/LevandaUI.php
+++ LevandaUI/src/NurAzliYT/LevandaUI/LevandaUI/LevandaUI.php
@@ -1,53 +1,52 @@
 <?php
 
-namespace NurAzliYT\LevandaUI\LevandaUI;
+namespace NurAzliYT\LevandaUI;
 
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
-
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
-
 use pocketmine\item\VanillaItems;
 use jojoe77777\FormAPI\SimpleForm;
 
 class LevandaUI extends PluginBase{
-    
-    public function onEnable():void{
+
+    public function onEnable(): void {
 
     }
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args):bool{
 
-        if($command->getName() == "levandaui"){
-            if($sender instanceof Player){
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+
+        if ($command->getName() == "levandaui") {
+            if ($sender instanceof Player) {
                 $this->onSimpleForm($sender);
             }
         }
         return true;
     }
 
-  public function onSimpleForm(Player $player):void{
-      $form = new SimpleForm(function (Player $player, $data){
-          if(!isset($data)){
-              return;
-          }
-          switch ($data){
-              case 0;
-                  $inv = $player->getInventory();
-                  $inv->setItem(index: 0, VanillaItems::DIAMOND()->setCount(64));
-                  $player->SendMessage("Kamu Telah Mendapatkan 64 Berlian!");
-                  break;
-              case 1;
-                  $inv = $player->getInventory();
-                  $inv->setItem(index: 0, VanillaItems::DIAMOND_ORE()->setCount(43));
-                  $player->SendMessage("Kamu Telah Mendapatkan 64 Berlian Metah!");
-                  break;
-          }
-      });
-      $form->setTitle("LevandaUI");
-      $form->setContent("Claim Your Rewards");
-      $form->addButton("OK");
-      $form->addButtom("Cencel");
-      $player->sendForm($form);
-  }
+    public function onSimpleForm(Player $player): void {
+        $form = new SimpleForm(function (Player $player, $data) {
+            if ($data === null) {
+                return;
+            }
+            switch ($data) {
+                case 0:
+                    $inv = $player->getInventory();
+                    $inv->setItem(0, VanillaItems::DIAMOND()->setCount(64));
+                    $player->sendMessage("Kamu Telah Mendapatkan 64 Berlian!");
+                    break;
+                case 1:
+                    $inv = $player->getInventory();
+                    $inv->setItem(0, VanillaItems::DIAMOND_ORE()->setCount(43));
+                    $player->sendMessage("Kamu Telah Mendapatkan 64 Berlian Metah!");
+                    break;
+            }
+        });
+        $form->setTitle("LevandaUI");
+        $form->setContent("Claim Your Rewards");
+        $form->addButton("OK");
+        $form->addButton("Cancel"); // Changed "Buttom" to "Button"
+        $player->sendForm($form);
+    }
 }
